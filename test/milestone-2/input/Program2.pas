9d7807a6fa749bc5be5c45b88d983c2e927e3b89
DitaMaheswari05 <ditamaheswari05@gmail.com>
2025-11-09 13:53:48 +0700

    feat : add testcase for parser

---
+++ test/milestone-2/input/Program2.pas
@@ -0,0 +1,14 @@
+program TestDeclarations;
+konstanta
+    MAX = 100;
+    MIN = 1;
+tipe
+    Range = 1..10;
+variabel
+    x, y, z: integer;
+    nilai: Range;
+mulai
+    x := MAX;
+    y := MIN;
+    z := x + y;
+selesai.
