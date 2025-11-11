0de68c122c3aa109acfd7f7e2fdfe58102f529b6
DitaMaheswari05 <ditamaheswari05@gmail.com>
2025-11-11 16:23:30 +0700

    feat : add implementation for advance()

--- src/Parser/parser.py
+++ src/Parser/parser.py
@@ -11,3 +11,21 @@ class Parser:
     """
     Main Parser class untuk Pascal-S menggunakan Recursive Descent
     """
+    
+    def __init__(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.position = 0
+        self.current_token = tokens[0] if tokens else None
+        
+        # Initialize sub-parsers
+        self.declaration_parser = DeclarationParser(self)
+        self.expression_parser = ExpressionParser(self)
+        self.statement_parser = StatementParser(self)
+    
+    def advance(self):
+        """Maju ke token berikutnya"""
+        self.position += 1
+        if self.position < len(self.tokens):
+            self.current_token = self.tokens[self.position]
+        else:
+            self.current_token = None
